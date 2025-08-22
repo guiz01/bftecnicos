0ddb42f72b6a49d03c7d5ee3dae62c6f34fb9290
[dyad] <gui_brandaorq2@hotmail.com>
2025-08-22 14:25:48 -0300

    [dyad] Corrigindo erro de sintaxe no mapa - wrote 3 file(s)

--- src/components/TecnicosMap.tsx
+++ src/components/TecnicosMap.tsx
@@ -1,13 +1,11 @@
 "use client";
 
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'; // Removendo MapContainerProps e TileLayerProps
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
 // Fix for default icon issue with Leaflet and Webpack/Vite
-delete (L.Icon.Default.prototype as any)._get='_getIconUrl';
-
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
   iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
